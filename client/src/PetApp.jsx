4254ddeb874cf62e1ca6660a4cc0d3d4fb00eeee
Taylor Thornton <taylorthornton.texas@gmail.com>
2022-08-12 07:37:11 -0500

    Redo

---
+++ client/src/PetApp.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import axios from 'axios';
+
+const PetApp = function WhateverStupidName() {
+
+  return (
+    <div>
+      Pet App
+    </div>
+  );
+};
+
+export default PetApp;
---
+++ client/src/index.jsx
@@ -0,0 +1,18 @@
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+
+import PetApp from './PetApp';
+
+const root = createRoot(document.getElementById('root'));
+
+// Huzzah for jsx!
+const App = function WhateverStupidName() {
+  return (
+    <RecoilRoot>
+      <PetApp />
+    </RecoilRoot>
+  );
+};
+
+root.render(<App />);
